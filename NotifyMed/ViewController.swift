d4d47e75d0f50b99ac4258ba1606c956d0d0e64b
Julian <jchumtl@gmail.com>
2020-03-26 00:52:24 -0400

    Non-template code

---
+++ NotifyMed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotifyMed
+//
+//  Created by Julian on 2020-03-26.
+//  Copyright © 2020 Julian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
